2967cb44ff16c78f70477f62104a12a331900b63
Alex Partin <25892264+adpartin@users.noreply.github.com>
2021-03-04 20:42:20 -0600

    config with simple namespace

--- src/config.py
+++ src/config.py
@@ -8,21 +8,37 @@ from pathlib import Path
 
 fdir = Path(__file__).resolve().parent
 
-MAIN_APPDIR = fdir/'../apps'
-DATADIR = fdir/'../data'
-ANNOTATIONS_FILENAME = 'annotations.csv'
-TILES_DIR = DATADIR/'tiles_png'
-TFR_DIR = DATADIR/'tfrecords'
-
 cfg = types.SimpleNamespace()
-cfg.MAIN_APPDIR = MAIN_APPDIR
-cfg.DATADIR = DATADIR
-cfg.ANNOTATIONS_FILENAME = ANNOTATIONS_FILENAME
-cfg.TILES_DIR = TILES_DIR
-cfg.TFR_DIR = TFR_DIR
+
+cfg.MAIN_APPDIR = (fdir/'../apps').resolve()
+cfg.DATADIR = (fdir/'../data').resolve()
+
+cfg.TILES_DIR = cfg.DATADIR/'tiles_png'
+
+cfg.ANNOTATIONS_FILENAME = 'annotations.csv'
+cfg.SF_ANNOTATIONS_FILENAME = 'annotations_slideflow.csv'
+
+cfg.TFR_DIR = (cfg.DATADIR/'tfrecords').resolve()
+# cfg.SF_TFR_DIR = (fdir/'../../slideflow-proj/PDX_FIXED').resolve()
+cfg.SF_TFR_DIR = (fdir/'../../slideflow-proj/PDX_FIXED_updated').resolve()
+
+# cfg.MAIN_APPDIR = MAIN_APPDIR
+# cfg.DATADIR = DATADIR
+# cfg.ANNOTATIONS_FILENAME = ANNOTATIONS_FILENAME
+# cfg.SF_ANNOTATIONS_FILENAME = SF_ANNOTATIONS_FILENAME
+# cfg.TILES_DIR = TILES_DIR
+# cfg.SF_TFR_DIR = SF_TFR_DIR
+
+# Meta file names
+# cfg.CROSSREF_FNAME = 'ImageID_PDMRID_CrossRef.csv'
+cfg.CROSSREF_FNAME = '_ImageID_PDMRID_CrossRef.xlsx'
+# cfg.PDX_META_FNAME = 'PDX_Meta_Information.csv'
+cfg.PDX_META_FNAME = 'PDX_Meta_Information.xlsx'
+cfg.SLIDES_META_FNAME = 'meta_from_wsi_slides.csv'
 
 # Slides that were identified as bad with either bad staining or poor quality. 
 # These notes were provided by Pearson's group in PDX_FIXED/slide_problems.txt
 cfg.BAD_SLIDES = [45983, 83742, 83743,  # poor quality
                   22232, 21836, 20729,  # staining off
                   ]
+# 10415, 13582, 9113,   # "no corresponding slide annotations" --> output from slideflow
