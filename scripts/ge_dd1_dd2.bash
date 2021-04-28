d674b44628cb56d32de54841c61fca6bf809f274
Alex Partin <25892264+adpartin@users.noreply.github.com>
2021-04-28 07:16:55 -0700

    update scripts and default_params

---
+++ scripts/ge_dd1_dd2.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Data name
+# dataname=tidy_all
+# dataname=tidy_partially_balanced
+# dataname=tidy_single_drug_all_samples
+dataname=tidy_drug_pairs_all_samples
+
+# Project name
+# prjname=bin_rsp_all
+# prjname=bin_rsp_partially_balanced
+# prjname=bin_rsp_single_drug_all_samples
+prjname=bin_rsp_drug_pairs_all_samples
+
+id_name=smp
+target=Response
+
+# split_on=slide
+split_on=Group
+
+# n_samples=60
+# n_samples=80
+# n_samples=100
+n_samples=-1
+
+DEVICE=$1
+echo "CUDA device: $DEVICE"
+
+trn_phase=$2
+# nn_arch=$3
+
+# CUDA_VISIBLE_DEVICES=$DEVICE python /vol/ml/apartin/projects/pdx-histo/src/trn_multimodal2.py \
+CUDA_VISIBLE_DEVICES=$DEVICE python src/trn_multimodal2.py \
+    --target $target \
+    --split_on $split_on \
+    --id_name $id_name \
+    --prjname $prjname \
+    --trn_phase $trn_phase \
+    --dataname $dataname \
+    --n_samples $n_samples \
+    --use_ge --use_dd1 --use_dd2
+
+    # --nn_arch $nn_arch \
+
